56f5e4eea43fea12788b6960e7b56e7197ae5344
Hashir Akbar <hashirakbar69@gmail.com>
2023-11-28 15:30:37 +0500

    completed content of sidebar and fixed some bugs

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ $(document).ready(() => {
    function menuToggler(clickItem, displayOne, icon) {
       $(displayOne).hide();
       $(clickItem).on("click", (e) => {
+         
+         
          $(displayOne).slideToggle()
+        
          if ($(icon).hasClass("icon-rotate") === false) {
             $(icon).addClass("icon-rotate")
 
@@ -27,21 +30,40 @@ $(document).ready(() => {
    menuToggler(".layouts", ".inner-list-layout", ".layouts-icon")
    menuToggler(".dashboard", ".inner-list-dashboard", ".dashboard-icon")
    menuToggler(".frontPages", ".inner-list-frontPages", ".frontPages-icon")
+   menuToggler(".ecommerce", ".inner-list-ecommerce", ".ecommerce-icon")
+   menuToggler(".academy", ".inner-list-academy", ".academy-icon")
+   menuToggler(".logistics", ".inner-list-logistics", ".logistics-icon")
+   menuToggler(".invoice", ".inner-list-invoice", ".invoice-icon")
+   menuToggler(".users", ".inner-list-users", ".invoice-users")
+   menuToggler(".rolesandpermissions", ".inner-list-rolesandpermissions", ".rolesandpermissions-icon")
+   menuToggler(".pages", ".inner-list-pages", ".pages-icon")
+   menuToggler(".authentication", ".inner-list-authentication", ".authentication-icon")
+   menuToggler(".wizard", ".inner-list-wizard", ".wizard-icon")
+   menuToggler(".cards", ".inner-list-cards", ".cards-icon")
+   menuToggler(".user-interface", ".inner-list-user-interface", ".user-interface-icon")
+   menuToggler(".extended-ui", ".inner-list-extended-ui", ".extended-ui-icon")
+   menuToggler(".icons", ".inner-list-icons", ".icons-icon")
+   menuToggler(".charts", ".inner-list-charts", ".charts-icon")
 
-   function windowClick(clickItem, item,) {
+   function windowClick(clickItem, item) {
 
       $(document).on("click", function (event) {
          let $a = $(item);
          let $b = $(clickItem);
+   
+
 
          if ($a !== event.target && !$a.has(event.target).length && $b !== event.target && !$b.has(event.target).length) {
             $a.hide();
-         }
+         } 
       })
    }
 
 
    function tooltip(clickItem, displayOne, close) {
+
+
+      let cc =  $(displayOne)
       $(displayOne).hide();
 
       $(clickItem).on("click", (e) => {
@@ -50,10 +72,12 @@ $(document).ready(() => {
       })
       windowClick(clickItem, displayOne)
 
-      $(close).click((e) => {
-         $(displayOne).show();
-         $(".parent").remove()
-         e.stopPropagation()
+      $(close).on("click", (e) => {
+         $(e.target).closest(".parent").addClass("remove");           
+      
+         $(displayOne).toggle()
+
+         
       })
 
    }
@@ -62,6 +86,7 @@ $(document).ready(() => {
    tooltip(".theme-change", ".theme-change-tooltip")
    tooltip(".customize", ".customize-change-tooltip")
    tooltip(".notification", ".notification-tooltip", ".close-icon")
+
 
 
 
